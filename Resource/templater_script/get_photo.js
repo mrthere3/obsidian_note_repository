bd58fe816f3088d328122df05d0fb4084a79df31
mrthere3 <871780968@qq.com>
2025-08-23 16:59:40 +0800

    vault backup: 2025-08-23 16:59:40

--- Resource/templater_script/get_photo.js
+++ Resource/templater_script/get_photo.js
@@ -4,8 +4,9 @@
  * @HomePage        : https://github.com/dmscode/Obsidian-Templates
  */
 async function get_photo() {
-  const response = await fetch("https://bing.img.run/rand.php")
-  const imgurl = await response.url
+  const response = await fetch("https://bing.biturl.top/")
+  let data = await response.json();
+  let imgurl = data.url;
   return imgurl
 }
 module.exports = get_photo; 
